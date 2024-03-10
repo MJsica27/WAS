6628d886c36e7736d9597f5e8d9161c064e07fb5
Kyle Matthew M. Quitco <kylequitco3212@gmail.com>
2024-03-10 23:07:18 +0800

    Finalize profile page's layout and features 1. Finalize profile page's design and layout 2. Implemented features:    - Edit Account Details    - Edit Email Address    - Edit Personal Information    - Change Password 3. Redid Implementation:    - Logout (Now requires confirmation before logging out)    - Delete Account (Now required password for delete confirmation)

--- iMan2Project/was/urls.py
+++ iMan2Project/was/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path("login/", views.user_login, name="login"),
     path("register/", views.register, name="register"),
     path('profile/', views.profile, name='profile'),
+    path('profile/edit_username', views.edit_username, name='edit_username'),
+    path('profile/edit_email', views.edit_email, name='edit_email'),
+    path('profile/edit_personal_info', views.edit_personal_info, name='edit_personal_info'),
+    path('profile/change_password', views.change_password, name='change_password'),
     path('delete_account/', views.delete_account, name='delete_account'),
     path('logout/', views.logout_user, name='logout'),
 ]
--- iMan2Project/was/views.py
+++ iMan2Project/was/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import update_session_auth_hash
 
 
 def user_login(request):
@@ -36,23 +37,25 @@ def register(request):
 
         # checks if the inputted passwords match
         if password != confirm_password:
-            messages.error(request, 'Passwords do not match.')
+            messages.error(request, 'Passwords do not match')
             return render(request, 'register.html')
 
         # check if the inputted username exists
         if User.objects.filter(username=username).exists():
-            messages.error(request, 'Username already exists.')
+            messages.error(request, 'Username already taken')
         # check if the inputted email exists
         elif User.objects.filter(email=email).exists():
-            messages.error(request, 'Email already exists.')
+            messages.error(request, 'Email already taken')
         # creates the User object
         else:
-            user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+            user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name,
+                                            last_name=last_name)
             user.save()
             messages.success(request, 'Registration successful.')
             return redirect('login')
 
     return render(request, 'register.html')
+
 
 def profile(request):
     if request.user.is_authenticated:
@@ -60,14 +63,116 @@ def profile(request):
     else:
         return redirect('login')
 
+
+def edit_username(request):
+    if request.method == 'POST':
+        new_username = request.POST.get('new_username')
+        current_username = request.user.username
+
+        if new_username == current_username:
+            messages.error(request, 'You already took that username')
+            return redirect('profile')
+        elif User.objects.filter(username=new_username).exists():
+            messages.error(request, 'Username already taken')
+            return redirect('profile')
+        else:
+            user = request.user
+            user.username = new_username
+            user.save()
+            messages.success(request, 'Username updated')
+            return redirect('profile')
+    else:
+        messages.error(request, 'Error occurred')
+        return redirect('profile')
+
+
+def edit_email(request):
+    if request.method == 'POST':
+        new_email = request.POST.get('new_email')
+        current_email = request.user.email
+
+        if new_email == current_email:
+            messages.error(request, 'You are already using that email')
+            return redirect('profile')
+        elif User.objects.filter(email=new_email).exists():
+            messages.error(request, 'Email already taken')
+            return redirect('profile')
+        else:
+            user = request.user
+            user.email = new_email
+            user.save()
+            messages.success(request, 'Email has been updated')
+            return redirect('profile')
+    else:
+        messages.error(request, 'Error occurred')
+        return redirect('profile')
+
+
+def edit_personal_info(request):
+    if request.method == 'POST':
+        new_first_name = request.POST.get('first_name')
+        new_last_name = request.POST.get('last_name')
+
+        current_first_name = request.user.first_name
+        current_last_name = request.user.last_name
+
+        if new_first_name == current_first_name and new_last_name == current_last_name:
+            messages.error(request, 'You are already using the first name and last name')
+            return redirect('profile')
+        else:
+            user = request.user
+            user.first_name = new_first_name
+            user.last_name = new_last_name
+            user.save()
+            messages.success(request, 'Personal information has been updated')
+            return redirect('profile')
+    else:
+        messages.error(request, 'Error occurred')
+        return redirect('profile')
+
+
+def change_password(request):
+    if request.method == 'POST':
+        old_password = request.POST.get('old_password')
+        new_password = request.POST.get('new_password')
+        confirm_new_password = request.POST.get('confirm_new_password')
+
+        if new_password != confirm_new_password:
+            messages.error(request, 'New passwords do not match')
+            return redirect('profile')
+        elif old_password == new_password:
+            messages.error(request, 'Old password and new password are the same')
+            return redirect('profile')
+        else:
+            user = request.user
+            if not user.check_password(old_password):
+                messages.error(request, 'Old password is incorrect')
+                return redirect('profile')
+            else:
+                user.set_password(new_password)
+                user.save()
+                update_session_auth_hash(request, user)
+                messages.success(request, 'Password changed successfully')
+                return redirect('profile')
+    else:
+        messages.error(request, 'Error occurred')
+        return redirect('profile')
+
+
 def delete_account(request):
     if request.method == 'POST':
-        if request.user.is_authenticated:
+        password = request.POST.get('password')
+
+        if not request.user.check_password(password):
+            messages.error(request, 'Password is incorrect')
+            return redirect('profile')
+        else:
             request.user.delete()
             logout(request)
             return redirect('login')
 
     return redirect('profile')
+
 
 def logout_user(request):
     logout(request)
